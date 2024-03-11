637535591dc515b103b98981828263a6bee37e1f
Mathew <126032483+MathewAnderson6706@users.noreply.github.com>
2024-03-11 15:21:10 -0400

    First Commit

---
+++ MathewModule8/src/main/java/mathew/anderson/n01436706/AndersonActivity8.java
@@ -0,0 +1,14 @@
+package mathew.anderson.n01436706;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AndersonActivity8 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
