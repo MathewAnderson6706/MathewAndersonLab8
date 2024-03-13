c07f1deaf0b8b24e85377053e6ff448cc93af50b
Mathew <126032483+MathewAnderson6706@users.noreply.github.com>
2024-03-13 17:35:56 -0400

    About Us Fragment Forgot about the toggle button it is now implemented

--- MathewModule8/src/main/java/mathew/anderson/n01436706/AboutFragment.java
+++ MathewModule8/src/main/java/mathew/anderson/n01436706/AboutFragment.java
@@ -2,6 +2,7 @@ package mathew.anderson.n01436706;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,8 +10,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class AboutFragment extends Fragment {
 
@@ -38,6 +41,20 @@ public class AboutFragment extends Fragment {
         TextView checkboxTextView = view.findViewById(R.id.matBoxCheck);
         TextView emailTextView = view.findViewById(R.id.matEmailCheck);
         TextView idTextView = view.findViewById(R.id.matIDCheck);
+
+        ToggleButton toggleButton = view.findViewById(R.id.matToggleButton);
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // Lock orientation to portrait
+                    getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                } else {
+                    // Set to auto orientation
+                    getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+                }
+            }
+        });
 
 
         if (isChecked) {
