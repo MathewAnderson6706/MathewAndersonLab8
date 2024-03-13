c755bbfcdfeeb5af9c078ad103336337a1559eb2
Mathew <126032483+MathewAnderson6706@users.noreply.github.com>
2024-03-13 16:55:57 -0400

    About Us Fragment -Background Pink -Toast with counter -2 textviews with my name

--- MathewModule8/src/main/java/mathew/anderson/n01436706/AboutFragment.java
+++ MathewModule8/src/main/java/mathew/anderson/n01436706/AboutFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class AboutFragment extends Fragment {
 
@@ -14,10 +15,20 @@ public class AboutFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private int counter = 0;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        counter++;
+        String message = getString(R.string.name) + " " + counter;
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
