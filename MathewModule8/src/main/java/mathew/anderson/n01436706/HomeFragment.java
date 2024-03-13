abfbb7f4db5e4927f187f67dc214d2a5af15e5eb
Mathew <126032483+MathewAnderson6706@users.noreply.github.com>
2024-03-12 23:10:13 -0400

    HomeFragment up and working

--- MathewModule8/src/main/java/mathew/anderson/n01436706/HomeFragment.java
+++ MathewModule8/src/main/java/mathew/anderson/n01436706/HomeFragment.java
@@ -7,6 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class HomeFragment extends Fragment {
 
@@ -14,10 +18,34 @@ public class HomeFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private ImageView imageView;
+    private Button button;
+    private int imageIndex = 0;
+    private int clickCount = 1;
+    private final int[] imageResources = {R.drawable.image1, R.drawable.image2, R.drawable.bunni, R.drawable.aklogo1};
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        imageView = view.findViewById(R.id.imageView);
+        button = view.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Update ImageView with the next image
+                imageView.setImageResource(imageResources[imageIndex]);
+                // Rotate to the next image
+                imageIndex = (imageIndex + 1) % imageResources.length;
+
+                Snackbar.make(view, getString(R.string.name) + clickCount, Snackbar.LENGTH_SHORT).show();
+
+                clickCount++;
+            }
+        });
+
+        return view;
     }
 }
